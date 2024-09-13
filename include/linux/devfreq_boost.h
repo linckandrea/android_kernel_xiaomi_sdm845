f2f9b443e574d2c2e768f566125763a0de75ad0f
linckandrea <linckandrea01@gmail.com>
2024-09-13 10:10:18 +0200

    devfreq_boost: kang from proton_bluecross

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2018-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
@@ -8,13 +8,14 @@
 #include <linux/devfreq.h>
 
 enum df_device {
-	DEVFREQ_CPU_LLCC_DDR_BW,
+	DEVFREQ_MSM_CPUBW,
 	DEVFREQ_MAX
 };
 
 #ifdef CONFIG_DEVFREQ_BOOST
 void devfreq_boost_kick(enum df_device device);
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
+void devfreq_boost_kick_wake(enum df_device device);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
 #else
 static inline
@@ -23,6 +24,10 @@ void devfreq_boost_kick(enum df_device device)
 }
 static inline
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
+{
+}
+static inline
+void devfreq_boost_kick_wake(enum df_device device)
 {
 }
 static inline
